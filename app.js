9183e20de66e6405f52427c8c13005638700153a
Mitchell <mitchellr3p@gmail.com>
2024-01-12 23:01:42 +0100

    add code

---
+++ app.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector(".toggle-btn");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+toggleBtn.addEventListener("click", function () {
+  dropdownMenu.classList.toggle("open");
+});
