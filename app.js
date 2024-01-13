69a506fd1fd49592905be3f8f9687a6b29a1b46c
Mitchell <mitchellr3p@gmail.com>
2024-01-13 23:50:23 +0100

    add code

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const dropdownMenu = document.querySelector(".dropdown-menu");
 toggleBtn.addEventListener("click", function () {
   dropdownMenu.classList.toggle("open");
 });
+
+let section1Btn = document.querySelector(".sec1-Mbtn");
+let section1popUp = document.querySelector(".sec1-popup");
+let section1close = document.querySelector(".close-btn");
+
+section1Btn.addEventListener('click', function() {
+  section1popUp.classList.add('switch-on')
+});
+
+section1close.addEventListener('click', function() {
+  section1popUp.classList.remove('switch-on');
+});
